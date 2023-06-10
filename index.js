fc294a6ff4094e5bb8a337e59a92bdaccf4a089c
badrmerchant <badr_merchant@yahoo.com>
2023-06-10 06:13:54 +0500

    add chapter 2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var a = "ss";
+a === "ss";
+a;
 console.log("web3.0C");
+var inventor = "Hedy Lamarr";
+inventor.toUpperCase(); // Ok: string
+//inventor.toFixed();
+var scientist = Math.random() > 0.5
+    ? 888
+    : "gg";
+if (scientist === 888) {
+    scientist.toFixed(); // Ok
+    console.log("true", scientist);
+}
+else {
+    scientist;
+    console.log("false", scientist);
+}
+// Type of scientist: number | string
+//scientist.toUpperCase();
+console.log("?", scientist);
--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
+let a="ss";a === "ss" ;a
 console.log("web3.0C");
+let inventor: number | string = "Hedy Lamarr";
+inventor.toUpperCase(); // Ok: string
+//inventor.toFixed();
+let scientist = Math.random() > 0.5
+? 888
+: "gg";
+if (scientist === 888) {
+
+scientist.toFixed(); // Ok
+console.log("true",scientist);
+
+}
+else{
+    scientist
+    console.log("false",scientist);
+    
+}
+// Type of scientist: number | string
+//scientist.toUpperCase();
+console.log("?",scientist);
+
